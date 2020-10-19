96fa66d203222cab793a612984ec63831baef9d9
Maksim Yurau <2004softwaredeveloper@gmail.com>
2020-10-19 17:40:08 +0300

    Added dto, interceptor, a lot of classes of storages and services, a lot of .jsp views. Added Likes/Dislikes/Comments.

---
+++ src/main/java/by/andrew/WebConfig.java
@@ -0,0 +1,28 @@
+package by.andrew;
+
+import by.andrew.interceptor.CreatePostInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "by.andrew")
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/pages/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CreatePostInterceptor()).addPathPatterns("/post");
+    }
+}
